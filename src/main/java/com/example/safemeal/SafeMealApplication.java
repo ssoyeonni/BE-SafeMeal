6a016049b74830476317b0a95f99fbaab1663370
ssoyeonni <165532198+ssoyeonni@users.noreply.github.com>
2025-11-14 19:11:07 +0900

    first commit

---
+++ src/main/java/com/example/safemeal/SafeMealApplication.java
@@ -0,0 +1,13 @@
+package com.example.safemeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeMealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeMealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/safemeal/SafeMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safemeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeMealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
